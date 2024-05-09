7f591268cf4926550209221cbae9167d5a1150fd
JD <32849950+JDBraun@users.noreply.github.com>
2024-05-09 13:58:18 -0500

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    databricks = {
+      source  = "databricks/databricks"
+      version = "~> 1.35.0"
+    }
+  }
+}
+
+provider "databricks" {
+  host          = "https://accounts.cloud.databricks.com"
+  account_id    = var.databricks_account_id
+  client_id     = var.client_id
+  client_secret = var.client_secret
+}
